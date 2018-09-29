dfba995306ad955b4cff6605647effe8d98c199c
MonologueX <3280795699@qq.com>
2018-09-29 22:42:41 +0800

    1 2

---
+++ 课堂练习/Project3/test.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include <windows.h>
+
+//typedef unsigned int unit;
+//
+//enum Sex
+//{
+//	nan,
+//	nv,
+//	baomi
+//};
+
+int main()
+{
+	const int i = 2;
+	i = 3;
+	//unsigned int age = 1;
+	//printf("%d ", nan);
+	//printf("%d ", nv);
+	//printf("%d ", baomi);
+	//auto int f = 4;
+	//char name[20];
+
+	//float f = 0.0;
+	//double ff = 0.0;
+	//long l = 0;
+	//int i = 0;
+	//char a = 'a';
+	//printf("%d ", sizeof(a));
+
+	//printf("%d ", sizeof(i));
+
+	//printf("%d ", sizeof(f));
+	//printf("%d ", sizeof(ff));
+	//printf("%d ", sizeof(l));
+
+	system("pause");
+	return 0;
+}
